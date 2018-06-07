fb9aae0782d4465644821b5240a1292de0e932ba
chj <chjfth@gmail.com>
2018-06-08 07:50:48 +0800

    created super-user: admin 123456

--- Lv2_first_project/first_project/settings.py
+++ Lv2_first_project/first_project/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+xxx_AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
