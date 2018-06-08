32cad195858fcc045d5254758cd8edb8fd131f79
chj <chjfth@gmail.com>
2018-06-08 08:04:19 +0800

    class WebpageAdmin(admin.ModelAdmin): # add this 	list_display = ['topic', 'name', 'url']
    admin.site.register(Topic)
    admin.site.register(Webpage, WebpageAdmin) # add this
    admin.site.register(AccessRecord)

--- Lv2_first_project/first_app/admin.py
+++ Lv2_first_project/first_app/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from first_app.models import Topic,Webpage,AccessRecord
+
+class WebpageAdmin(admin.ModelAdmin):
+	list_display = ['topic', 'name', 'url']
+
 admin.site.register(Topic)
-admin.site.register(Webpage)
+admin.site.register(Webpage, WebpageAdmin)
 admin.site.register(AccessRecord)
