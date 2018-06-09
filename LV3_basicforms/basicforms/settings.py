e3bcff244dcbdc6b1cba437e6936274fc219fd05
chj <chjfth@gmail.com>
2018-06-09 09:47:07 +0800

    Serving static files(bootstrap.min.css 4.1.0) locally.
    In settings.py:
    
    STATICFILES_DIRS = [
        os.path.join(BASE_DIR, "static"),
    ]

--- LV3_basicforms/basicforms/settings.py
+++ LV3_basicforms/basicforms/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+xxx_AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
